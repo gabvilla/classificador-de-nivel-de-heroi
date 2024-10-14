92959b7511e7a46b523327784b1cad6d9c21b658
gvillassanti <gabriel.villassanti2@hotmail.com>
2024-10-13 22:24:26 -0300

    feat: add initial project files

---
+++ script.js
@@ -0,0 +1,9 @@
+const xpInput = document.querySelector("#heroXp");
+const nameInput = document.querySelector("#heroName");
+const verifyButton = document.querySelector("#verify-btn");
+const form = document.querySelector(".form")
+
+form.addEventListener(("submit"), (e) => {
+   e.preventDefault();
+   console.log(xpInput.value)
+, false})
